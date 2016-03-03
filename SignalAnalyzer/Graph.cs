4ce5f93c673beeeac51ed1ce0e89833f941fcd7e
kiefer7126 <b1012046@fun.ac.jp>
2016-03-03 15:13:06 +0900

    extract method form Graph.cs

--- SignalAnalyzer/Graph.cs
+++ SignalAnalyzer/Graph.cs
@@ -16,7 +16,6 @@ namespace SignalAnalyzer
         private int plotData, red, green, blue;
 
         private int xZero, yZero, xMax, yMax, marginRight, marginLeft, marginTop, marginBottom, gramWidth, gramHeight;
-        private Graphics g;
         private Font myFont;
         private Pen spectrogramPen;
         private int penSize;
@@ -32,7 +31,7 @@ namespace SignalAnalyzer
 
             //X軸最小値、最大値、目盛間隔の設定
             chartcontrol.ChartAreas["ChartArea1"].AxisX.Minimum = 0;
-            chartcontrol.ChartAreas["ChartArea1"].AxisX.Maximum =data.Length;
+            chartcontrol.ChartAreas["ChartArea1"].AxisX.Maximum = data.Length;
             chartcontrol.ChartAreas["ChartArea1"].AxisX.Interval = 441000;
 
             //Y軸最小値、最大値、目盛間隔の設定
@@ -56,7 +55,7 @@ namespace SignalAnalyzer
             series.ChartType = SeriesChartType.Line;
 
             //グラフのデータを追加
-            for (int i = 0; i < data.Length; i += 441) series.Points.AddXY(i,data[i]);
+            for (int i = 0; i < data.Length; i += 441) series.Points.AddXY(i, data[i]);
 
             //作ったSeriesをchartコントロールに追加する
             chartcontrol.Series.Add(series);
@@ -100,7 +99,7 @@ namespace SignalAnalyzer
         {
             Console.WriteLine("Draw Spectrogram...");
 
-            penSize = 5; 
+            penSize = 5;
             xStep = 1;
             yStep = 1;
 
@@ -141,8 +140,19 @@ namespace SignalAnalyzer
             g.DrawString(yLabel, myFont, Pens.Black.Brush, 5, gramHeight / 2);
         }
 
+        public void drawBeatComponent(FrequencyAnalyzer freq, Graphics g, double[] beat)
+        {
+            //ピーク検出
+            //for (int i = 0; i < beat.Length; i++) g.DrawLine(Pens.Black, (xZero + i), yZero, (xZero + i), yZero - (int)(beat[i]*20000));
+            //                for (int i = 0; i < beat.Length; i++) g.DrawLine(Pens.Black, (xZero + i), yZero, (xZero + i), yZero - (int)(beat[i] * 200));
+            //立ち上がり成分
+            for (int i = 0; i < beat.Length; i++) g.DrawLine(Pens.Black, (xZero + i), yZero, (xZero + i), yZero - (int)(beat[i] * 250));
+        }
+
         public void DrawSpectrogram(PictureBox picture, FrequencyAnalyzer freq, int[] metric, double[][] divStftData, int beatInterval, ProgressBar progressBar)
         {
+            Graphics g;
+
             initSpectrogram(picture);
 
 
@@ -157,14 +167,12 @@ namespace SignalAnalyzer
                 myFont = new Font("Arial", 9);
                 spectrogramPen = new Pen(Color.FromArgb(0, 0, 0), penSize);
 
-                var pen2 = new Pen(Color.FromArgb(0, 0, 0), penSize);
                 var boundaryPen = new Pen(Color.FromArgb(200, 0, 0), 10);
 
                 //グラフの描画
 
                 float bottomUp, dataIntervalNomalization;
                 float penSizeHalf = penSize / 2;
-                float inverseSTFTLength = (float)(1.0 / freq.stftData.Length);
 
                 for (int time = 1; time < freq.stftData.Length - 1; time++)
                 {
@@ -198,173 +206,13 @@ namespace SignalAnalyzer
                 }
 
                 drawAxis(g, picture);
-     
-                /* ビートを描画したいときに使用 */
-                
+
                 var beatDetection = new BeatDetection();
                 var beat = beatDetection.main(freq);
 
-                //ピーク検出
-                //for (int i = 0; i < beat.Length; i++) g.DrawLine(Pens.Black, (xZero + i), yZero, (xZero + i), yZero - (int)(beat[i]*20000));
-//                for (int i = 0; i < beat.Length; i++) g.DrawLine(Pens.Black, (xZero + i), yZero, (xZero + i), yZero - (int)(beat[i] * 200));
-                //立ち上がり成分
-               // for (int i = 0; i < beat.Length; i++) g.DrawLine(Pens.Black, (xZero + i), yZero, (xZero + i), yZero - (int)(beat[i]/5));
-                
-                /*拍節構造の描画*/
-                if(metric != null)
-                {
-                    for (int i = 0; i < metric.Length; i++)
-                    {
-                        //g.DrawLine(Pens.Black, (xZero + i), yZero, (xZero + i), yZero - metric[i] );
-                    }
+                //drawBeatComponent(freq, g, beat);
 
-                    var positiveStftData = new double[divStftData.Length][];
-                    int[] minMatrix = Enumerable.Repeat(0, divStftData.Length).ToArray();
-
-                    for (int i = 0; i < minMatrix.Length; i++ )
-                    {
-                        minMatrix[i] = Math.Abs((int)divStftData[i].Min());
-                    }
-
-                        //正の値にマッピング
-                    for (int i = 0; i < positiveStftData.Length; i++)
-                        {
-                            double[] positiveStftFreq = Enumerable.Repeat(0.0, freq.focusFreqLength).ToArray(); //すべて0で初期化
-
-                            for (int j = 0; j < positiveStftFreq.Length; j++)
-                            {
-                                positiveStftFreq[j] = divStftData[i][j] + minMatrix.Max(); //最小値の絶対値の最大値
-                            }
-                            positiveStftData[i] = positiveStftFreq;
-                        }
-
-                    var peakStftData = freq.peakDetection(positiveStftData);
-
-                   for (int i = 0; i < divStftData.Length; i++)
-                   {
-                       for (int j = 0; j < freq.focusFreqLength; j++)
-                       {
-                           //時間軸で分割したスペクトルの描画
-                         // g.DrawLine(Pens.Black, xZero + (beatInterval * (i) + 108), yZero - j, xZero + (beatInterval * (i) + 108) + (int)positiveStftData[i][j]/500, yZero - j);
-
-                          //時間軸で分割したスペクトルのピークの描画
-                          //g.DrawLine(Pens.Black, xZero + (beatInterval * (i)), yZero - j, xZero + (beatInterval * (i)) + (int)peakStftData[i][j] / 100, yZero - j);
-                       }
-                   }
-
-                   int changeRatio = 0;
-
-                   var changeRatioData = new int[positiveStftData.Length - 1];
-                   changeRatioData = freq.changeRatio(peakStftData);
-
-                   var sumPower = freq.sumSpectoroPower(positiveStftData);
-
-                    for (int i = 0; i < changeRatioData.Length; i++)
-                    {
-                        //changeRatio = (int)(changeRatioData[i] * 500 /((sumPower[i]+sumPower[i+1])/2));
-                        //changeRatio = (int)(changeRatioData[i] * 500 / sumPower[i]);
-                        //changeRatio = (int)(changeRatioData[i] / 5000);
-                        changeRatio = (int)(changeRatioData[i]/300);
-
-                        //if (changeRatio > 210) g.DrawLine(boundaryPen, xZero + (beatInterval * (i) + 10), yZero, xZero + (beatInterval * (i) + 10), yMax); //グルーピング協会
-                        g.DrawLine(pen2, xZero + (beatInterval) * (i) + 10, yMax, xZero + (beatInterval) * (i) + 10, yMax + changeRatio);
-                        g.DrawLine(pen2, xZero + (beatInterval * (i) + 10), yMax+100, xZero + (beatInterval * (i) + 10), yMax +100+ (int)(sumPower[i] / 500));
-                    }
-
-
-                    var gpr2a = new double[sumPower.Length];
-
-                    gpr2a[0] = 0;
-
-                    for (int i = 1; i < gpr2a.Length - 1; i++)
-                    {
-                        /*
-                        if (sumPower[i] < sumPower[i - 1] && sumPower[i] < sumPower[i + 1]) gpr2a[i] = 1.0 - (sumPower[i] / sumPower.Max());
-                        else gpr2a[i] = 0.0;
-                        */ 
-
-                        gpr2a[i] = 1.0 - (sumPower[i] / sumPower.Max());
-                    }
-
-                    for (int i = 0; i < gpr2a.Length - 1; i++)
-                    {
-                       // g.DrawLine(pen2, xZero + beatInterval * i +10, yMax, xZero + beatInterval*i + 10, yMax + (int)(gpr2a[i] * 5000));   
-                    }
-
-                    ArrayList beatIntervalArray = new ArrayList();
-                    int oldBeat = 0;
-                    int newBeatInterval = 0;
-                    //ビート間隔を格納
-                    for (int i = 0; i < beat.Length; i++)
-                    {
-                        if (beat[i] > 0.0)
-                        {
-                            newBeatInterval = i - oldBeat;
-                            oldBeat = i;
-                            beatIntervalArray.Add(newBeatInterval);
-                        }
-                    }
-
-                    var gpr2b = new double[beatIntervalArray.Count];
-
-                    gpr2b[0] = 0;
-
-                    for(int i = 1; i < gpr2b.Length-1; i++)
-                    {
-                        
-                        if ((int)beatIntervalArray[i] > (int)beatIntervalArray[i - 1] && (int)beatIntervalArray[i] > (int)beatIntervalArray[i + 1]) gpr2b[i] = 1.0;
-                        else gpr2b[i] = 0.0;
-                    }
-
-                    int x = 10;
-                    for (int i = 0; i < gpr2b.Length-1; i++)
-                    {
-                        //g.DrawLine(pen2, xZero + x, yMax, xZero + x, yMax + gpr2b[i]*500);
-                        x += (int)beatIntervalArray[i+1];
-                    }
-
-                    var gpr3 = new double[changeRatioData.Length+1];
-
-                    gpr3[0] = 0;
-
-                    for (int i = 1; i < gpr3.Length - 1; i++)
-                    {
-                        gpr3[i] = (double)changeRatioData[i - 1] / (double)changeRatioData.Max();
-                    }
-
-                    var gpr5 = new double[sumPower.Length];
-
-                    for (int i = 0; i < gpr5.Length; i++)
-                    {
-                        if (i <= gpr5.Length/2) gpr5[i] = (2.0 * i) / gpr5.Length;
-                        else gpr5[i] = 2 -(2.0 * i) / gpr5.Length;
-                    }
-
-                    /*
-                    for (int i = 0; i < gpr5.Length; i++)
-                    {
-                        g.DrawLine(pen2, xZero + beatInterval * i, yZero, xZero + beatInterval * i, yZero - (int)(gpr5[i] * 200));
-                    }*/
-
-                    double s2a = 1.0;
-                    double s2b = 1.0;
-                    double s3  = 0.2;
-                    var groupingBoundary = new double[sumPower.Length];
-                    for(int i = 0; i<groupingBoundary.Length; i++)
-                    {
-                        groupingBoundary[i] = gpr2a[i] * s2a +/* gpr2b[i] * s2b + */gpr3[i] * s3;  
-                    }
-                   
-                    for(int i = 0; i<groupingBoundary.Length; i++)
-                    {
-                        groupingBoundary[i] = groupingBoundary[i] * gpr5[i] / groupingBoundary.Max();
-                    }
-
-                    for (int i = 0; i < groupingBoundary.Length; i++)
-                    {
-                        g.DrawLine(pen2, xZero + beatInterval * i , yZero, xZero + beatInterval * i , yZero - (int)(groupingBoundary[i] * 200)); 
-                    }
-                }
+                if (metric != null) drawMetricStructure(g, metric, divStftData, freq, beatInterval, beat);
 
                 //Graphicsリソース解放
                 g.Dispose();
@@ -373,6 +221,163 @@ namespace SignalAnalyzer
             catch (Exception e)
             {
 
+            }
+        }
+
+        public void drawMetricStructure(Graphics g, int[] metric, double[][] divStftData, FrequencyAnalyzer freq, int beatInterval, double[] beat)
+        {
+            var metricPen = new Pen(Color.FromArgb(0, 0, 0), penSize);
+
+            for (int i = 0; i < metric.Length; i++)
+            {
+                //g.DrawLine(Pens.Black, (xZero + i), yZero, (xZero + i), yZero - metric[i] );
+            }
+
+            var positiveStftData = new double[divStftData.Length][];
+            int[] minMatrix = Enumerable.Repeat(0, divStftData.Length).ToArray();
+
+            for (int i = 0; i < minMatrix.Length; i++)
+            {
+                minMatrix[i] = Math.Abs((int)divStftData[i].Min());
+            }
+
+            //正の値にマッピング
+            for (int i = 0; i < positiveStftData.Length; i++)
+            {
+                double[] positiveStftFreq = Enumerable.Repeat(0.0, freq.focusFreqLength).ToArray(); //すべて0で初期化
+
+                for (int j = 0; j < positiveStftFreq.Length; j++)
+                {
+                    positiveStftFreq[j] = divStftData[i][j] + minMatrix.Max(); //最小値の絶対値の最大値
+                }
+                positiveStftData[i] = positiveStftFreq;
+            }
+
+            var peakStftData = freq.peakDetection(positiveStftData);
+
+            for (int i = 0; i < divStftData.Length; i++)
+            {
+                for (int j = 0; j < freq.focusFreqLength; j++)
+                {
+                    //時間軸で分割したスペクトルの描画
+                    // g.DrawLine(Pens.Black, xZero + (beatInterval * (i) + 108), yZero - j, xZero + (beatInterval * (i) + 108) + (int)positiveStftData[i][j]/500, yZero - j);
+
+                    //時間軸で分割したスペクトルのピークの描画
+                    //g.DrawLine(Pens.Black, xZero + (beatInterval * (i)), yZero - j, xZero + (beatInterval * (i)) + (int)peakStftData[i][j] / 100, yZero - j);
+                }
+            }
+
+            int changeRatio = 0;
+
+            var changeRatioData = new int[positiveStftData.Length - 1];
+            changeRatioData = freq.changeRatio(peakStftData);
+
+            var sumPower = freq.sumSpectoroPower(positiveStftData);
+
+            for (int i = 0; i < changeRatioData.Length; i++)
+            {
+                //changeRatio = (int)(changeRatioData[i] * 500 /((sumPower[i]+sumPower[i+1])/2));
+                //changeRatio = (int)(changeRatioData[i] * 500 / sumPower[i]);
+                //changeRatio = (int)(changeRatioData[i] / 5000);
+                changeRatio = (int)(changeRatioData[i] / 300);
+
+                //if (changeRatio > 210) g.DrawLine(boundaryPen, xZero + (beatInterval * (i) + 10), yZero, xZero + (beatInterval * (i) + 10), yMax); //グルーピング協会
+                g.DrawLine(metricPen, xZero + (beatInterval) * (i) + 10, yMax, xZero + (beatInterval) * (i) + 10, yMax + changeRatio);
+                g.DrawLine(metricPen, xZero + (beatInterval * (i) + 10), yMax + 100, xZero + (beatInterval * (i) + 10), yMax + 100 + (int)(sumPower[i] / 500));
+            }
+
+
+            var gpr2a = new double[sumPower.Length];
+
+            gpr2a[0] = 0;
+
+            for (int i = 1; i < gpr2a.Length - 1; i++)
+            {
+                /*
+                if (sumPower[i] < sumPower[i - 1] && sumPower[i] < sumPower[i + 1]) gpr2a[i] = 1.0 - (sumPower[i] / sumPower.Max());
+                else gpr2a[i] = 0.0;
+                */
+
+                gpr2a[i] = 1.0 - (sumPower[i] / sumPower.Max());
+            }
+
+            for (int i = 0; i < gpr2a.Length - 1; i++)
+            {
+                // g.DrawLine(metricPen, xZero + beatInterval * i +10, yMax, xZero + beatInterval*i + 10, yMax + (int)(gpr2a[i] * 5000));   
+            }
+
+            ArrayList beatIntervalArray = new ArrayList();
+            int oldBeat = 0;
+            int newBeatInterval = 0;
+            //ビート間隔を格納
+            for (int i = 0; i < beat.Length; i++)
+            {
+                if (beat[i] > 0.0)
+                {
+                    newBeatInterval = i - oldBeat;
+                    oldBeat = i;
+                    beatIntervalArray.Add(newBeatInterval);
+                }
+            }
+
+            var gpr2b = new double[beatIntervalArray.Count];
+
+            gpr2b[0] = 0;
+
+            for (int i = 1; i < gpr2b.Length - 1; i++)
+            {
+
+                if ((int)beatIntervalArray[i] > (int)beatIntervalArray[i - 1] && (int)beatIntervalArray[i] > (int)beatIntervalArray[i + 1]) gpr2b[i] = 1.0;
+                else gpr2b[i] = 0.0;
+            }
+
+            int x = 10;
+            for (int i = 0; i < gpr2b.Length - 1; i++)
+            {
+                //g.DrawLine(metricPen, xZero + x, yMax, xZero + x, yMax + gpr2b[i]*500);
+                x += (int)beatIntervalArray[i + 1];
+            }
+
+            var gpr3 = new double[changeRatioData.Length + 1];
+
+            gpr3[0] = 0;
+
+            for (int i = 1; i < gpr3.Length - 1; i++)
+            {
+                gpr3[i] = (double)changeRatioData[i - 1] / (double)changeRatioData.Max();
+            }
+
+            var gpr5 = new double[sumPower.Length];
+
+            for (int i = 0; i < gpr5.Length; i++)
+            {
+                if (i <= gpr5.Length / 2) gpr5[i] = (2.0 * i) / gpr5.Length;
+                else gpr5[i] = 2 - (2.0 * i) / gpr5.Length;
+            }
+
+            /*
+            for (int i = 0; i < gpr5.Length; i++)
+            {
+                g.DrawLine(metricPen, xZero + beatInterval * i, yZero, xZero + beatInterval * i, yZero - (int)(gpr5[i] * 200));
+            }*/
+
+            double s2a = 1.0;
+            double s2b = 1.0;
+            double s3 = 0.2;
+            var groupingBoundary = new double[sumPower.Length];
+            for (int i = 0; i < groupingBoundary.Length; i++)
+            {
+                groupingBoundary[i] = gpr2a[i] * s2a +/* gpr2b[i] * s2b + */gpr3[i] * s3;
+            }
+
+            for (int i = 0; i < groupingBoundary.Length; i++)
+            {
+                groupingBoundary[i] = groupingBoundary[i] * gpr5[i] / groupingBoundary.Max();
+            }
+
+            for (int i = 0; i < groupingBoundary.Length; i++)
+            {
+                g.DrawLine(metricPen, xZero + beatInterval * i, yZero, xZero + beatInterval * i, yZero - (int)(groupingBoundary[i] * 200));
             }
         }
 
@@ -394,19 +399,19 @@ namespace SignalAnalyzer
                     green = 0;
                     blue = 0 + (plotHsvData % 255);
                     break;
-               
+
                 case 1:
                     red = 0;
                     green = 0 + (plotHsvData % 255);
                     blue = 255;
                     break;
-        
+
                 case 2:
                     red = 0;
                     green = 255;
                     blue = 255 - (plotHsvData % 255);
                     break;
-                   
+
                 case 3:
                     red = 0 + (plotHsvData % 255);
                     green = 255;
